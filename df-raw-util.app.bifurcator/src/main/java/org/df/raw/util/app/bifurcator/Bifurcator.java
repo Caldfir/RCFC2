f70aed5298175580919263f96e2ed6de6df1d8e9
Caldfir <caldfir@hotmail.com>
2016-03-25 15:58:29 -0700

    add bifurcator project

---
+++ df-raw-util.app.bifurcator/src/main/java/org/df/raw/util/app/bifurcator/Bifurcator.java
@@ -0,0 +1,13 @@
+package org.df.raw.util.app.bifurcator;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bifurcator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
