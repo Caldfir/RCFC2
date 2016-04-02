1024bbdd8c8ff548c0e6f51ef8ac529a9e601e50
Caldfir <caldfir@hotmail.com>
2016-04-02 10:53:20 -0700

    use new parser/composer structure for reading/writing tags

--- df-raw-util.app/df-raw-util.app.formatter/src/main/java/caldfir/df_raw_util/app/formatter/Formatter.java
+++ df-raw-util.app/df-raw-util.app.formatter/src/main/java/caldfir/df_raw_util/app/formatter/Formatter.java
@@ -1,6 +1,8 @@
 package caldfir.df_raw_util.app.formatter;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Writer;
 
@@ -8,9 +10,12 @@ import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import caldfir.df_raw_util.core.compose.RawTagComposer;
+import caldfir.df_raw_util.core.compose.TagComposer;
 import caldfir.df_raw_util.core.config.IOConfig;
 import caldfir.df_raw_util.core.config.RelationshipConfig;
-import caldfir.df_raw_util.core.parse.TreeBuilder;
+import caldfir.df_raw_util.core.parse.RawTagParser;
+import caldfir.df_raw_util.core.parse.TagParser;
 import caldfir.df_raw_util.core.primitives.Tag;
 import caldfir.df_raw_util.core.relationship.RelationshipMap;
 import caldfir.df_raw_util.ui.FileProgressFrame;
@@ -43,27 +48,37 @@ public class Formatter {
       // read and parse
       display.set("reading " + shortName, 2 * i + 1);
       Tag root = null;
+      TagParser parser = null;
       try {
-        TreeBuilder t = new TreeBuilder(fileList[i].getPath(), relFileMap);
-        root = t.getRoot();
+        parser = new RawTagParser(
+            new BufferedReader(new FileReader(fileList[i])), relFileMap);
+        root = parser.parse();
       } catch (IOException e) {
         LOG.error(e.toString());
         continue;
+      } finally {
+        try {
+          parser.close();
+        } catch (IOException e){
+          // no-op
+        }
       }
 
       // write
       display.set("writing " + shortName, 2 * i);
-      Writer writer = null;
+      TagComposer composer = null;
       try {
-        writer = ioConfig.buildOutputWriter(shortName);
-        root.writeRawFile(shortName, writer);
+        Writer w = ioConfig.buildOutputWriter(shortName);
+        composer = new RawTagComposer(w);
+        composer.compose(root);
       } catch (IOException e) {
         LOG.error(e.toString());
+        continue;
       } finally {
         try {
-          writer.close();
-        } catch (Exception e) {
-          LOG.error(e.toString());
+          composer.close();
+        } catch (IOException e) {
+          // no-op
         }
       }
     }
