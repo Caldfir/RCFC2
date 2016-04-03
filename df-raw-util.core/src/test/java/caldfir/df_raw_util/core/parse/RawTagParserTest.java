d118bd2b52045439f9fbff9d2899f118910c0cdf
Caldfir <caldfir@hotmail.com>
2016-04-03 01:11:50 -0700

    add raw tag parser test case

---
+++ df-raw-util.core/src/test/java/caldfir/df_raw_util/core/parse/RawTagParserTest.java
@@ -0,0 +1,37 @@
+package caldfir.df_raw_util.core.parse;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Test;
+
+import caldfir.df_raw_util.core.primitives.Tag;
+import caldfir.df_raw_util.core.relationship.BoolRelationshipMap;
+
+public class RawTagParserTest {
+
+  public static final String RAW_ONE = "[OBJECT:CREATURE]";
+  public static final String RAW_TWO = "[CREATURE:OCELOT]";
+  
+  @Test
+  public void testParseOneElement() {
+    RawTagParser rtp =
+        new RawTagParser(
+            new StringReader(RAW_ONE),
+            new BoolRelationshipMap(true));
+    
+    Tag tag = rtp.parse();
+    
+    try {
+      rtp.close();
+    } catch (IOException e) {
+      // no-op
+    }
+    
+    assertTrue(tag.getArgument(0).equals("OBJECT"));
+    assertTrue(tag.getArgument(1).equals("CREATURE"));
+    assertTrue(tag.getNumChildren() == 0);
+  }
+}
